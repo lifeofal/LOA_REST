4893456ee45deea30097f5c4241a7b7fc75d2d68
Alejandro Cervantes <46929938+lifeofal@users.noreply.github.com>
2023-03-26 16:17:47 -0400

    Update LOAAPIApplication.java

--- src/main/java/com/herokuapp/lifeofal/rest/LOAAPIApplication.java
+++ src/main/java/com/herokuapp/lifeofal/rest/LOAAPIApplication.java
@@ -3,12 +3,15 @@ package com.herokuapp.lifeofal.rest;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.bson.types.ObjectId;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class LOAAPIApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
